1e6863781920479817abcc4fccf888f83e8323e6
Kessius00 <kes.olling00@gmail.com>
2022-04-18 22:30:12 +0200

    first commit

--- TTT.py
+++ TTT.py
@@ -1,17 +1,15 @@
-import numpy as np
-
-board1 = [' ']*10
-
+#board1 = [' ']*10
+board2 = ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 
 
 #functies
-def boardmaker(board):
+def boardlayout(board):
     #making a board/layout of the values in variable board1 from index 1-9
-    print(board[1]+'|'+board[2]+'|'+board[3])
-    print('-|-|-')
-    print(board[4]+'|'+board[5]+'|'+board[6])
-    print('-|-|-')
-    print(board[7]+'|'+board[8]+'|'+board[9])
+    print(board[1],'|',board[2],'|',board[3])
+    print('- | - | -')
+    print(board[4],'|',board[5],'|',board[6])
+    print('- | - | -')
+    print(board[7],'|',board[8],'|',board[9])
 
 
 def playerchoose():
@@ -28,34 +26,28 @@ def playerchoose():
         player2 = 'X'
     return (player1, player2)
 
-player1, player2 = playerchoose()
+#player1, player2 = playerchoose()
 
 
-def detector():
-    #should detect if there is a winner or not
-    pass
+def pickcell(board):
+    #asks and returns the cell the player wants to mutate
 
-def takingturns(board):
-    #makes sure every player takes its turn 
-    for i in range(9):
-        if i%2 == 0:
-            print('Player 1, do your move. ')
-            picksystem(board,player1)
-        
-        else:
-            print('Player 2, move. ')
-            picksystem(board,player2)
-        
-
-def picksystem(board, player):
-    
-    cell = ' '
-    #choosing the cell
-    
+    #choosing the row with a while loop
     row = input('Which row do you want to mutate? ')
+
+    while row.isnumeric() == False and row not in range(1,4):
+        row = input('Which row do you want to mutate? ')
+    
+    #choosing the column with a while loop
     column = input('Which column do you want to mutate? ')
-    
-    
+
+    while column.isnumeric() == False and column not in range(1,4):
+        column = input('Which column do you want to mutate? ')
+
+    row = int(row)
+    column = int(column)
+
+    #converting the rows and columns into cells in the board
     if row == 1:
         cell = board[column]
     
@@ -64,24 +56,58 @@ def picksystem(board, player):
 
     elif row == 3:
         cell = board[column+6]
-
-    return cell
-
-
-
-
     
-'''
-    while cell != ' ':
-        print('That cell is already in use. Try again. ')
-        row = input('Which row do you want to mutate? ')
-        column = input('Which column do you want to mutate? ')
-        if row == 1:
-            cell = board[column]
+    celindex = (board.index(cell))
 
-        elif row == 2:
-            cell = board[column+3]
+    return celindex, cell
 
-        elif row == 3:
-            cell = board[column+6]
-'''
+
+def boxchecker(board):
+    #checks if the cell picked is empty and can be mutated, if not, pickcell
+    celindex, cell = pickcell(board)
+
+    while cell != int:
+        celindex, cell = pickcell(board)
+    
+    return celindex, cell
+
+
+def mutator(board, player):
+    #mutates the board for the given cell
+
+    celindex = boxchecker(board)
+
+    board[celindex] = player
+    
+    return board
+
+
+def playing(board):
+
+    player1, player2 = playerchoose()
+
+    boardlayout(board)
+
+    for i in range(9):
+        if i%2 == 0:
+            print('Player 1, do your move. ')
+            mutator(board, player1)
+            boardlayout(board2)
+        
+        else:
+            print('Player 2, move. ')
+            mutator(board, player2)
+            boardlayout(board2)
+    
+    
+
+playing(board2)
+
+
+
+
+def detector():
+    #should detect if there is a winner or not
+    pass
+
+ 
